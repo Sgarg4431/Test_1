c4362e1e9fb0472e1b85aff154d6a46b0979c292
sgarg4431 <sgarg4431@gmail.com>
2023-07-16 00:13:15 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import axios from 'axios';
+import QRCode from 'qrcode-generator'
 
 function App() {
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [result, setResult] = useState('');
+  function generateQRCode(url) {
+    const qr = QRCode(0, 'H');
+    qr.addData(url);
+    qr.make();
+  
+    const qrCodeImage = qr.createDataURL(10, 4);
+    const qrCodeImgElement = document.createElement('img');
+    qrCodeImgElement.src = qrCodeImage;
+  
+    // Append the QR code image to the document body
+    document.body.appendChild(qrCodeImgElement);
+  }
+  
+  const handleSubmit = async (event) => {
+  
+    event.preventDefault();
+  
+    try {
+      // Prepare API request
+      const apiUrl = 'https://api.blockpay.co.za/merchant/initiatePaymentRequest'; // Replace with your API endpoint
+      const requestData = {
+        k: input1,
+        a: input2
+      };
+  
+      // Make API call using Axios
+      const response = await axios.post(apiUrl, requestData);
+  
+      // Process API response
+      setResult(JSON.stringify(response.data));
+      console.log(response.data);
+      generateQRCode(response.data.url);
+    } catch (error) {
+      // Handle error
+      console.error('API Error:', error);
+    }
+  };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Example</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="input1">API key</label>
+        <input type="text" id="input1" value={input1} onChange={(e) => setInput1(e.target.value)} required />
+        <br />
+        <label htmlFor="input2">Amount</label>
+        <input type="text" id="input2" value={input2} onChange={(e) => setInput2(e.target.value)} required />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+
+      <div id="result">{result}</div>
     </div>
   );
 }
